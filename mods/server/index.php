c4f70b40ffd6d8e6da9fdbf8a0174eb436c40cf5
jesterjunk <fake@null.zilch>
2023-07-27 18:53:26 -0500

    ...

--- mods/server/index.php
+++ mods/server/index.php
@@ -18,13 +18,36 @@
         --color-00: hsl(70, 8%, 15%);
         --color-01: hsl(50, 12%, 20%);
         --color-02: silver;
+        --color-03: hsl(50, 11%, 41%);  /* used for github corner fill and scrollbar thumb  */
 
         --virustotal-background-color: hsl(72, 8%, 12%);
 
-        --header-background-color: hsl(72, 9%, 11%);
+        --header-background-color: hsl(72, 9%, 11%);  /* used for scrollbar track  */
         --table-header-text-color: #BAB8AE;
 
         --table-row-spacing-adjustment: 42px;
+    }
+
+    html {
+        scroll-behavior: smooth;
+        scroll-padding: calc(202px + 28px);
+
+/*                            This is for                                     */
+/*                              FireFox                                       */
+/*                       thumb           track                                */
+        scrollbar-color: var(--color-03) var(--header-background-color);
+    }
+
+    body::-webkit-scrollbar {
+        width: 1em;
+    }
+
+    body::-webkit-scrollbar-track {
+        background-color: var(--header-background-color);
+    }
+
+    body::-webkit-scrollbar-thumb {
+        background-color: var(--color-03);
     }
 
     body {
@@ -62,7 +85,7 @@
         height: calc(180px + 22px);
         background-color: var(--header-background-color);
         border-bottom: 1px solid hsl(80, 8%, 8%);
-    filter: drop-shadow(0 0 8px hsla(0, 0%, 0%, .5));
+        filter: drop-shadow(0 0 8px hsla(0, 0%, 0%, .5));
     }
 
 
@@ -116,8 +139,7 @@
     }
 
     table {
-        position: absolute;
-        top: 0;
+        top: 400px;
         margin: 0 auto 0;
         width: calc(100% - 48px);
         border-spacing: 0 var(--table-row-spacing-adjustment);
@@ -214,9 +236,9 @@
     }
     div.sha256_hash {
         flex: 19;
-        margin-bottom: 0;
     }
     div.sha256_hash textarea {
+        margin-bottom: -3px;
         overflow: hidden;
         color: var(--color-02);
         border: none;
@@ -527,7 +549,7 @@ HTML;
 
         <div id="github-corner">
             <a href="https://github.com/jesterjunk/WurmUnlimited" target="_blank" class="github-corner" title="View source on GitHub" aria-label="View source on GitHub">
-                <svg width="80" height="80" viewBox="0 0 250 250" style="fill: hsl(50, 11%, 41%); color: var(--header-background-color); position: absolute; top: 0; border: 0; left: 0; transform: scale(-1, 1)" aria-hidden="true">
+                <svg width="80" height="80" viewBox="0 0 250 250" style="fill: var(--color-03); color: var(--header-background-color); position: absolute; top: 0; border: 0; left: 0; transform: scale(-1, 1)" aria-hidden="true">
                     <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
                     <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path>
                     <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
